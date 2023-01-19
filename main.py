3bbd33c2a886ff269455c8c9b5ed80c9b26a3aa0
Андрей Савостьянов <ratio123@yandex.ru>
2023-01-20 00:37:14 +0300

    Add Warrior, Mage, Healer class.

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from random import randint
 # Global variable
 DEFAULT_ATTACK = 5
 DEFAULT_DEFENCE = 10
+DEFAULT_STAMINA = 80
 
 
 class Character:
@@ -39,17 +40,29 @@ class Character:
 
 class Warrior(Character):
     """Class Warrior"""
-    def __init__(self) -> None:
-        super().__init__()
+    BRIEF_DESC_CHAR_CLASS = (' дерзкий воин ближнего боя. '
+                             'Сильный, выносливый и отважный')
+    RANGE_VALUE_ATTACK = (3, 5)
+    RANGE_VALUE_DEFENCE = (5, 10)
+    SPECIAL_BUFF = DEFAULT_STAMINA + 25
+    SPECIAL_SKILL = 'Выносливость'
 
 
 class Mage(Character):
     """Class Mage"""
-    def __init__(self) -> None:
-        super().__init__()
+    BRIEF_DESC_CHAR_CLASS = (' находчивый воин дальнего боя. '
+                             'Обладает высоким интеллектом')
+    RANGE_VALUE_ATTACK = (5, 10)
+    RANGE_VALUE_DEFENCE = (-2, 2)
+    SPECIAL_BUFF = DEFAULT_ATTACK + 40
+    SPECIAL_SKILL = 'Атака'
 
 
 class Healer(Character):
     """Class Healer"""
-    def __init__(self) -> None:
-        super().__init__()
+    BRIEF_DESC_CHAR_CLASS = (' могущественный заклинатель. '
+                             'Черпает силы из природы, веры и духов')
+    RANGE_VALUE_ATTACK = (-3, -1)
+    RANGE_VALUE_DEFENCE = (2, 5)
+    SPECIAL_BUFF = DEFAULT_DEFENCE + 30
+    SPECIAL_SKILL = 'Защита'
